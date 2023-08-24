767cf4be7a0b75946e7140c47a56777759b2d800
RinaI <ivanisevicrina1999@gmail.com>
2023-08-24 12:01:09 +0200

    Prikaz slika zupljanina- radi!

--- src/main/java/com/example/projekt2023java/PrikazSlikaZupljaninaController.java
+++ src/main/java/com/example/projekt2023java/PrikazSlikaZupljaninaController.java
@@ -4,7 +4,9 @@ import baza.BazaPodataka;
 import entitet.Slike;
 import entitet.Zupljanin;
 import javafx.collections.FXCollections;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.ListView;
 import javafx.scene.image.Image;
@@ -15,9 +17,7 @@ import java.io.FileInputStream;
 import java.io.ObjectInputStream;
 import java.sql.*;
 import java.time.LocalDate;
-import java.util.List;
-import java.util.Objects;
-import java.util.Properties;
+import java.util.*;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
@@ -39,10 +39,12 @@ public class PrikazSlikaZupljaninaController {
     @FXML
     private ComboBox<String> odabirZupljaninaComboBox;
 
+    @FXML
+    private Button nextButton;
 
-
-
+    private List<byte[]> imagesList;
     private int currentIndex;
+
     private static Connection connectToDatabase() throws Exception {
         Properties konfiguracijaBaze = new Properties();
         konfiguracijaBaze.load(new FileInputStream("dat/bazaPodataka.properties"));
@@ -58,6 +60,7 @@ public class PrikazSlikaZupljaninaController {
     public void initialize() {
         List<String> zupljaninList = BazaPodataka.dohvatiSveZupljane().stream().map(s -> s.getIme() + " " + s.getPrezime()).toList();
         odabirZupljaninaComboBox.setItems(FXCollections.observableList(zupljaninList));
+        nextButton.setDisable(true); // Disable the "Next" button initially
 
     }
 
@@ -71,26 +74,47 @@ public class PrikazSlikaZupljaninaController {
                 ovajZupljanin = zupljanin;
             }
         }
+
+        imagesList = new ArrayList<>();
         try (Connection connection = DriverManager.getConnection("jdbc:h2:tcp://localhost/~/java-tvz-2023-PROJEKT", "student", "student")) {
             String query = "SELECT slika FROM ZupljaninSlike WHERE zupljanin_id = ?";
             PreparedStatement statement = connection.prepareStatement(query);
-            statement.setLong(1, ovajZupljanin.getId());  // zupljanin_id
+            statement.setLong(1, ovajZupljanin.getId());
 
             ResultSet resultSet = statement.executeQuery();
-            if (resultSet.next()) {
+            while (resultSet.next()) {
                 byte[] slikaBytes = resultSet.getBytes("slika");
-                Image image = new Image(new ByteArrayInputStream(slikaBytes));
-                imageView.setImage(image);
+                imagesList.add(slikaBytes);
             }
 
             resultSet.close();
             statement.close();
+            currentIndex = 0; // Initialize index for images
+            if (!imagesList.isEmpty()) {
+                setImageInView();
+                nextButton.setDisable(false); // Enable the "Next" button
+            } else {
+                imageView.setImage(null); // No images found, clear the image view
+                nextButton.setDisable(true); // Disable the "Next" button
+            }
         } catch (SQLException e) {
             e.printStackTrace();
         }
 
 
 
+    }
+
+    @FXML
+    private void showNextImage(ActionEvent event) {
+        currentIndex = (currentIndex + 1) % imagesList.size();
+        setImageInView();
+    }
+
+    private void setImageInView() {
+        byte[] slikaBytes = imagesList.get(currentIndex);
+        Image image = new Image(new ByteArrayInputStream(slikaBytes));
+        imageView.setImage(image);
     }
 
 
