376290645e8df4605b60365caec3cd1c19412b9d
RinaI <ivanisevicrina1999@gmail.com>
2023-09-03 21:56:55 +0200

    Unos zupnih obavijest2

--- src/main/java/com/example/projekt2023java/UnosObavijestiController.java
+++ src/main/java/com/example/projekt2023java/UnosObavijestiController.java
@@ -1,10 +1,15 @@
 package com.example.projekt2023java;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class UnosObavijestiController {
 
@@ -22,15 +27,55 @@ public class UnosObavijestiController {
         String subotaObavijest = subotaTextField.getText();
         String nedjeljaObavijest = nedjeljaTextField.getText();
 
-        try (FileWriter writer = new FileWriter("dat/obavijesti.txt")) {
-            writer.write(radnimDanObavijest + "\n");
-            writer.write(subotaObavijest + "\n");
-            writer.write(nedjeljaObavijest + "\n");
+        // Read the existing content from the file
+        List<String> existingContent = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader("dat/obavijesti.txt"))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                existingContent.add(line);
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        // Optionally, provide feedback to the admin that the data has been saved.
-        System.out.println("Obavijesti su spremljene.");
+        Boolean promjena = false;
+
+        if (!radnimDanObavijest.isEmpty()) {
+            existingContent.set(0, radnimDanObavijest);
+            promjena=true;
+        }
+        if (!subotaObavijest.isEmpty()) {
+            existingContent.set(1, subotaObavijest);
+            promjena=true;
+        }
+        if (!nedjeljaObavijest.isEmpty()) {
+            existingContent.set(2, nedjeljaObavijest);
+            promjena=true;
+        }
+
+        // Write the modified content back to the file
+        try (FileWriter writer = new FileWriter("dat/obavijesti.txt")) {
+            for (String line : existingContent) {
+                writer.write(line + "\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        if(promjena) {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Spremljeno");
+            alert.setHeaderText("Uspješno spremljeno");
+            alert.setContentText("Dodana nova vremena za mise!");
+            alert.showAndWait();
+        }else{
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Nema promjene");
+            alert.setHeaderText("Niste unjeli ništa");
+            alert.setContentText("Morate unjeti nešto ako želite promjenu");
+            alert.showAndWait();
+        }
+
     }
+
 }
